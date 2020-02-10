d4d7c719a77346dc656a5ad48156260a528052b6
xuzhougeng <xuzhougeng@yeah.net>
2020-02-10 15:24:36 +0800

    第二版

--- Week_01/G20200343030533/LeetCode_66_33.c
+++ Week_01/G20200343030533/LeetCode_66_33.c
@@ -1,6 +1,9 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+
+//第一次写的代码
+
 int* plusOne(int* digits, int digitsSize, int* returnSize){
 
     // 如果末尾小于9
@@ -31,4 +34,32 @@ int* plusOne(int* digits, int digitsSize, int* returnSize){
     res[0] = 1;
     memcpy(res+1, digits, sizeof(int) * digitsSize);
     return res;
+}
+
+//第二次写的代码
+int* plusOne(int* digits, int digitsSize, int* returnSize){
+
+    if ( digitsSize == 0) return ; 
+
+    // 从后往前
+    // 先算最后一位
+    int sum = digits[digitsSize - 1] + 1;
+    digits[digitsSize-1] = sum % 10;
+    int carry = sum / 10;
+    for ( int i = digitsSize - 2; i >= 0; i--){
+        sum = digits[i] + carry;
+        digits[i] = sum % 10;
+        carry = sum / 10;
+    }
+    if (carry > 0 ){
+        int *res = (int *)malloc(sizeof(int) * (digitsSize+1));
+        res[0] = carry;
+        memcpy(res+1, digits, sizeof(int) * digitsSize);
+        *returnSize =( digitsSize + 1);
+        return res;
+
+    }
+    *returnSize = digitsSize ;
+    return digits;
+
 }
