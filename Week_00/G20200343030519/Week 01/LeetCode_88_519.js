8585e314e1721db5f9ee694d3f68a8c8556f3ccf
huoyitong <349247397@qq.com>
2020-02-14 12:49:22 +0800

    dev: dev

---
+++ Week_00/G20200343030519/Week 01/LeetCode_88_519.js
@@ -0,0 +1,14 @@
+// https://leetcode-cn.com/problems/merge-sorted-array/
+
+var merge = function(nums1, m, nums2, n) {
+    let len1 = m - 1;
+    let len2 = n - 1;
+    let len = m + n - 1;
+    while (len1 >= 0 && len2 >= 0) {
+        nums1[len--] = nums1[len1] > nums2[len2] ? nums1[len1--] : nums2[len2--];
+    }
+    function arrayCopy(src, srcIndex, dest, destIndex, length) {
+        dest.splice(destIndex, length, ...src.slice(srcIndex, srcIndex + length));
+    }
+    arrayCopy(nums2, 0, nums1, 0, len2 + 1);
+};
