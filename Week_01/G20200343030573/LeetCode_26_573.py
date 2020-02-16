8001b56962da38750a81532f8d1b1f462fc8f104
tmac <wangjianzse@qq.com>
2020-02-16 22:57:30 +0800

    week01

---
+++ Week_01/G20200343030573/LeetCode_26_573.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        x = 1
+        for i in range(len(nums) - 1):
+            if (nums[i] != nums[i + 1]):
+                nums[x] = nums[i + 1]
+                x += 1
+        return (x)
