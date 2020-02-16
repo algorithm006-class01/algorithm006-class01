e071b7e98b682d579f70e1e3eddbb644600ceebf
杨润炜 <runweiyang@tuputech.com>
2020-02-16 15:32:12 +0800

    feat: add LeetCode_189_387.js

---
+++ Week_01/G20200343030387/LeetCode_189_387.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    // 暴力解法
+    // 循环k次移动，每次遍历整个数组且每个元素旋转一位
+    // let temp, previous
+    // for (let i = 0; i < k; i++) {
+    //     previous = nums[nums.length - 1] // 当前要移到元素的上一个元素
+    //     for (let j = 0; j < nums.length; j++) {
+    //         temp = nums[j]
+    //         nums[j] = previous
+    //         previous = temp
+    //     }
+    // }
+
+    // 额外数组
+    // 空间复杂度为：O(n)
+    // let newArr = []
+    // let numsLen = nums.length
+    // for (let i = 0; i < numsLen; i++) {
+    //     newArr[(i+k)%numsLen] = nums[i]
+    // }
+    // for (let j = 0; j < newArr.length; j++) {
+    //     nums[j] = newArr[j]
+    // }
+
+    // 多次反转数组
+    k %= nums.length
+    reverse(nums, 0, nums.length - 1)
+    reverse(nums, 0, k - 1)
+    reverse(nums, k, nums.length - 1)
+}
+
+function reverse(arr, start, end) {
+    while (start < end) {
+        swap(arr, start, end)
+        start++
+        end--
+    }
+}
+function swap(arr, a, b) {
+    let temp = arr[a]
+    arr[a] = arr[b]
+    arr[b] = temp
+}
