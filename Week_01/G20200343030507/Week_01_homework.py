ba615ae36e171b9afae476e37dc062a5e12f5ca8
jervis-lai <55199514+jervis-lai@users.noreply.github.com>
2020-02-16 16:40:36 +0800

    modified test
    modified test

--- Week_01/G20200343030507/Week_01_homework.py
+++ Week_01/G20200343030507/Week_01_homework.py
@@ -38,7 +38,7 @@ class Solution:
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead. 
         """
         p1 = m - 1
         p2 = n - 1 
