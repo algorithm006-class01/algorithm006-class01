e629c7eb81643fbaedef5402b42833b221e8237e
VelvetMeng <18852851930@163.com>
2020-02-16 23:12:03 +0800

    添加 LeetCode_1_513.java

---
+++ Week_01/G20200343030513/LeetCode_1_513.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int index = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if ( nums[i-1] != nums[i]) {
+                nums[index] = nums[i];
+				index ++;
+            }
+        }
+        return index;
+    }
+}
