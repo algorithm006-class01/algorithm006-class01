38f828dc151afdd9d7dc0ec5a57be6c9056a7a69
dontaire <dontaire@pop-os.localdomain>
2020-02-16 17:37:10 +0800

    week 01 homework

---
+++ Week_01/G20200343030561/Leetcode_088_561.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=88 lang=java
+ *
+ * [88] 合并两个有序数组
+ */
+
+// @lc code=start
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p = m-- + n-- - 1;
+        while (m >= 0 && n >= 0)
+            nums1[p--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+
+            
+        while (n >= 0)
+            nums1[p--] = nums2[n--];
+    }
+}
+// @lc code=end
+
