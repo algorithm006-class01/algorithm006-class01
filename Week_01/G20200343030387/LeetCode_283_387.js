b0e350c53aa3a83011db18a62d137b7108c0e81c
杨润炜 <runweiyang@tuputech.com>
2020-02-16 23:45:57 +0800

    feat: add LeetCode_283_387.js

---
+++ Week_01/G20200343030387/LeetCode_283_387.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let j = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            if (i !== j) {
+                nums[j] = nums[i]
+                nums[i] = 0
+            }
+            j++
+        }
+    }
+}
