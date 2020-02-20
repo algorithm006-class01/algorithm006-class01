f6e02e1c68a78b4c87732dbfa62469fdce146a5e
安贞 <zhen.an@lanmaoly.com>
2020-02-21 07:06:21 +0800

    590

--- Week_02/G20200343030391/LeetCode_590_391.java
+++ Week_02/G20200343030391/LeetCode_590_391.java
@@ -28,25 +28,43 @@ public class LeetCode_590_391 {
     }
 
     public static List<Integer> postorderByLoop(Node root) {
-        ArrayList<Integer> result = new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        //前指针
+        Node pre = null;
         Stack<Node> stack = new Stack<>();
-        Node node = root;
-        stack.push(node);
-        while (!node.children.isEmpty()) {
-            for (int i = node.children.size() - 1; i >= 0; i--) {
-                stack.push(node.children.get(i));
-            }
-            node = stack.peek();
-        }
+        stack.push(root);
         while (!stack.isEmpty()) {
-            node = stack.pop();
-            result.add(node.val);
+            Node cur = stack.peek();
+            if ((cur.children.size() == 0) || (pre != null && cur.children.contains(pre))) {
+                //加入结果集
+                res.add(cur.val);
+                stack.pop();
+                //更新pre指针
+                pre = cur;
+            } else {
+                //继续压栈，注意压栈是从右往左
+                List<Node> nodeList = cur.children;
+                for (int i = nodeList.size() - 1; i >= 0; i--) {
+                    stack.push(nodeList.get(i));
+                }
+            }
         }
-        return result;
+        return res;
     }
 
     public static List<Integer> postorderByRecursion(Node root) {
-        return null;
+        ArrayList<Integer> result = new ArrayList<>();
+        recursion(root, result);
+        return result;
+    }
+    public static void recursion(Node node, ArrayList<Integer> list) {
+        if (node != null) {
+            for (Node child : node.children) {
+                recursion(child, list);
+            }
+            list.add(node.val);
+        }
     }
 
     public static class Node {
