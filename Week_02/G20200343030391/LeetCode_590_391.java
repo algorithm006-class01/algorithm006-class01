1abbda5240ac3bfb8b2ffb36e8fb1fd72fce9ee3
安贞 <zhen.an@lanmaoly.com>
2020-02-21 06:59:35 +0800

    144 & 590

--- Week_02/G20200343030391/LeetCode_590_391.java
+++ Week_02/G20200343030391/LeetCode_590_391.java
@@ -1,19 +1,20 @@
 package G20200343030391;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
 public class LeetCode_590_391 {
 
     public static void main(String[] args) {
-        Node node1 = new Node(1);
-        Node node2 = new Node(2);
-        Node node3 = new Node(3);
-        Node node4 = new Node(4);
-        Node node5 = new Node(5);
-        Node node6 = new Node(6);
-        node1.children = new ArrayList<Node>(){{
+        Node node1 = new Node(1,new ArrayList<>());
+        Node node2 = new Node(2,new ArrayList<>());
+        Node node3 = new Node(3,new ArrayList<>());
+        Node node4 = new Node(4,new ArrayList<>());
+        Node node5 = new Node(5,new ArrayList<>());
+        Node node6 = new Node(6,new ArrayList<>());
+        node1.children = new ArrayList<Node>() {{
             add(node3);
             add(node2);
             add(node4);
@@ -22,7 +23,7 @@ public class LeetCode_590_391 {
             add(node5);
             add(node6);
         }};
-        List<Integer> postorder = postorder(node1);
+        List<Integer> postorder = postorderByLoop(node1);
         System.out.println(postorder);
     }
 
@@ -30,17 +31,22 @@ public class LeetCode_590_391 {
         ArrayList<Integer> result = new ArrayList<>();
         Stack<Node> stack = new Stack<>();
         Node node = root;
-        while (node != null && !stack.isEmpty()) {
-            if (node != null) {
-                for (int i = 0; i < node.children.size(); i++) {
-
-                }
+        stack.push(node);
+        while (!node.children.isEmpty()) {
+            for (int i = node.children.size() - 1; i >= 0; i--) {
+                stack.push(node.children.get(i));
             }
+            node = stack.peek();
         }
+        while (!stack.isEmpty()) {
+            node = stack.pop();
+            result.add(node.val);
+        }
+        return result;
     }
 
     public static List<Integer> postorderByRecursion(Node root) {
-
+        return null;
     }
 
     public static class Node {
--- Week_02/G20200343030391/NOTE.md
+++ Week_02/G20200343030391/NOTE.md
@@ -1,1 +1,30 @@
 学习笔记
+# 树
+# 二叉树
+# 二叉搜索树
+# 图
+# 红黑树
+
+# 递归
+## 盗梦空间
+   - 模板
+      ```java
+      public void recur(int lever,int param) {
+        // terminator
+        if(leverl > max_level){
+            // process result
+            return;
+        }
+        //process current logic
+        process(level,param);
+        
+        //drill down
+        recur(level:level+1,newParam);
+        
+        //restore current status
+      }
+      ```
+   - 注意
+     1. 抵制人肉递归
+     2. 找最近重复性
+     3. 数学归纳法思维
