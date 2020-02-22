e7258c20972de80b46f785878fb5b671cd2fe8bb
安贞 <zhen.an@lanmaoly.com>
2020-02-22 08:22:33 +0800

    145二叉树后序遍历

--- Week_02/G20200343030391/LeetCode_145_391.java
+++ Week_02/G20200343030391/LeetCode_145_391.java
@@ -10,8 +10,14 @@ public class LeetCode_145_391 {
         TreeNode node1 = new TreeNode(1);
         TreeNode node2 = new TreeNode(2);
         TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
         node1.right = node2;
         node2.left = node3;
+        node3.left = node4;
+        node4.right = node5;
+        node5.right = node6;
         List<Integer> integers = postorderTraversallByLoop(node1);
         System.out.println(integers);
     }
@@ -26,8 +32,8 @@ public class LeetCode_145_391 {
     }
 
     /**
-     * 循环遍历：stack 保存未访问节点，node保存当前指向的节点（为空代表左子树到头），last代表上次访问的最后节点，栈顶右子树不为空
-     *          且上次循环最后访问节点！=栈顶的右节点代表当前节点是从右子树过来的
+     * 循环遍历：stack 保存未访问节点，node保存当前指向的节点（为空代表左子树到头），last代表上次访问的最后节点，栈顶元素右子树不为空
+     *          遍历右子树；上次循环最后访问节点！=栈顶元素右节点代表栈顶
      * 时间复杂度：O(n)
      * @param root
      * @return
