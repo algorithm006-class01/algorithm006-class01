55a2ca41fecb3973349598fbb02592300753b4b0
Wisee <1247539141@qq.com>
2020-02-23 19:38:52 +0800

    week02 505

---
+++ Week_02/G20200343030505/LeetCode_144_505.java
@@ -0,0 +1,23 @@
+class LeetCode_144_505 {
+    private LinkedList<Integer> result = new LinkedList<Integer>();
+    public List<Integer> postorder(Node root) {
+        if(root == null) {
+            return result;
+        }
+        
+        Stack<Node> stacks = new Stack<Node>();
+        stacks.add(root);
+        Node curr = null;
+        while (!stacks.isEmpty()) {
+            curr = stacks.pop();
+            result.addFirst(curr.val);
+            if (curr.children != null) {
+                for(Node node:curr.children) {
+                    stacks.add(node);
+                }
+            }
+        }
+
+        return result;
+    } 
+}
