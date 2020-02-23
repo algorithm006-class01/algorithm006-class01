693e09e681e4a5103d17451a195a5aa607cd09a6
caidongsen <1030683070@qq.com>
2020-02-23 19:09:54 +0800

    week 2

---
+++ Week_02/G20200343030617/LeetCode_1_617.go
@@ -0,0 +1,28 @@
+//236. 二叉树的最近公共祖先
+
+package main
+
+/**
+ * Definition for TreeNode.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *ListNode
+ *     Right *ListNode
+ * }
+ */
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == p || root == q || root == nil {
+		return root
+	}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+
+	if left != nil && right != nil {
+		return root
+	} else if left == nil {
+		return right
+	} else {
+		return left
+	}
+
+}
