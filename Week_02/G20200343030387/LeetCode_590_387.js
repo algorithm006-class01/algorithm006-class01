f685ff8b89301deeb1de08acdb374108905a4e98
杨润炜 <runweiyang@tuputech.com>
2020-02-23 12:20:48 +0800

    refactor:  LeetCode_590_387.js of week02

--- Week_02/G20200343030387/LeetCode_590_387.js
+++ Week_02/G20200343030387/LeetCode_590_387.js
@@ -6,21 +6,24 @@
  * };
  */
 // 系统栈递归
-const helper = function (root, res) {
-    if (root) {
-        if (root.children) {
-            for (let i = 0; i < root.children.length; i++) {
-                helper(root.children[i], res)
-            }
-        }
-        res.push(root.val)
-    }
-}
+// 后序遍历：先遍历完该节点的子树节点，再添加上根
 /**
  * @param {Node} root
  * @return {number[]}
  */
 var postorder = function (root) {
+    const helper = function (root, res) {
+        if (root) {
+            if (root.children) {
+                // 递归该子节点的所有子树节点
+                root.children.forEach(child => {
+                    helper(child, res)
+                })
+            }
+            // 添加上根节点
+            res.push(root.val)
+        }
+    }
     const res = []
     helper(root, res)
     return res
