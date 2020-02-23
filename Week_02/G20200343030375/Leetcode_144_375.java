4c2b9d91c5c7d5e8b1392dd56a56ac862da20377
majingyuan <imcking@126.com>
2020-02-23 21:33:16 +0800

    第二周的作业

--- Week_02/G20200343030375/Leetcode_144_375.java
+++ Week_02/G20200343030375/Leetcode_144_375.java
@@ -3,6 +3,10 @@ package G20200343030375;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 内存消耗 : 37.4 MB , 在所有 Java 提交中击败了 5.19% 的用户
+ */
+
 public class Leetcode_144_375 {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> result =  new ArrayList<>();
