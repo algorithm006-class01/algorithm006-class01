abd281e415c14ed0d45d54331ac3a401486c42f8
Peng Wu <pengwu8421@gmail.com>
2020-02-24 00:07:25 -0800

    46, 49

--- Week_02/G20200343030499/LeetCode_77_499.java
+++ Week_02/G20200343030499/LeetCode_77_499.java
@@ -7,7 +7,7 @@
 // 利用level order traversal的思路，不同的是到最后一层直接返回该层。
 // 可以利用LinkedList是Queue和List的子类的特性，当成Queue处理，完后可以直接当List返回
 // @lc code=start
-class Solution {
+class Solution_1 {
     public List<List<Integer>> combine(int n, int k) {
         LinkedList<List<Integer>> current = new LinkedList<>();
         for (int i = 1; i <= n; i++) {
@@ -33,6 +33,28 @@ class Solution {
             }
         }
         return bfs(n, k, currentQueue, depth + 1);
+    }
+}
+
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> results = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        recurser(n, k, results, result, 1);
+        return results;
+    }
+
+    private void recurser(int n, int k, List<List<Integer>> results, List<Integer> currentResult, int startNum) {
+        if (currentResult.size() == k) {
+            results.add(currentResult);
+            return;
+        }
+
+        for (int i = startNum; i <= n; i++) {
+            currentResult.add(i);
+            recurser(n, k, results, new ArrayList<>(currentResult), i + 1);
+            currentResult.remove(currentResult.size() - 1);
+        }
     }
 }
 // @lc code=end
