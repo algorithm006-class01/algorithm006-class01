91a75dbcb07254522731818d26d19b23a325a3a9
安贞 <zhen.an@lanmaoly.com>
2020-03-01 16:14:32 +0800

    127

--- Week_03/G20200343030391/LeetCode_127_391.java
+++ Week_03/G20200343030391/LeetCode_127_391.java
@@ -1,14 +1,121 @@
 package G20200343030391;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
 
 public class LeetCode_127_391 {
 
     public static void main(String[] args) {
 
+        String beginWord = "qa";
+        String endWord = "sq";
+        List<String> wordList = new ArrayList<>(
+                Arrays.asList("si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye"));
+        long start = System.currentTimeMillis();
+        int length = new LeetCode_127_391().ladderLength(beginWord, endWord, wordList);
+        System.out.println("耗时：" + (System.currentTimeMillis() - start) + " 结果：" + length);
     }
 
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        int end = wordList.indexOf(endWord);
+        if (end == -1) {
+            return 0;
+        }
+        wordList.add(beginWord);
+        int start = wordList.size() - 1;
+        Queue<Integer> queue1 = new LinkedList<>();
+        Queue<Integer> queue2 = new LinkedList<>();
+        Set<Integer> visited1 = new HashSet<>();
+        Set<Integer> visited2 = new HashSet<>();
+        queue1.offer(start);
+        queue2.offer(end);
+        visited1.add(start);
+        visited2.add(end);
+        int count = 0;
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            count++;
+            if (queue1.size() > queue2.size()) {
+                Queue<Integer> tmp = queue1;
+                queue1 = queue2;
+                queue2 = tmp;
+                Set<Integer> t = visited1;
+                visited1 = visited2;
+                visited2 = t;
+            }
+            int size1 = queue1.size();
+            while (size1-- > 0) {
+                String s = wordList.get(queue1.poll());
+                for (int i = 0; i < wordList.size(); ++i) {
+                    if (visited1.contains(i)) {
+                        continue;
+                    }
+                    if (!canConvert(s, wordList.get(i))) {
+                        continue;
+                    }
+                    if (visited2.contains(i)) {
+                        return count + 1;
+                    }
+                    visited1.add(i);
+                    queue1.offer(i);
+                }
+            }
+        }
         return 0;
     }
+
+    public int ladderLengthByBFS(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)) {
+            return 0;
+        }
+        boolean[] visited = new boolean[wordList.size()];
+        int index = wordList.indexOf(beginWord);
+        if (index != -1) {
+            visited[index] = true;
+        }
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            count++;
+            for (int i = 0; i < size; i++) {
+                String start = queue.poll();
+                for (int j = 0; j < wordList.size(); j++) {
+                    String s = wordList.get(j);
+                    if (visited[j]) {
+                        continue;
+                    }
+                    if (!canConvert(start, s)) {
+                        continue;
+                    }
+                    if (s.equals(endWord)) {
+                        return count + 1;
+                    }
+                    visited[j] = true;
+                    queue.offer(s);
+                }
+            }
+        }
+        return 0;
+    }
+
+    public boolean canConvert(String s1, String s2) {
+        int diffCount = 0;
+        for (int i = 0; i < s1.length(); ++i) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                ++diffCount;
+                if (diffCount > 1) {
+                    return false;
+                }
+            }
+        }
+        return diffCount == 1;
+    }
+
+
 }
