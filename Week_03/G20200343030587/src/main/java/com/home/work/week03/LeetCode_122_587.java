91993cf03d4a19ac07f55d5df9aef7ec8e12c0bb
zhchzh <278799636@qq.com>
2020-03-01 21:33:24 +0800

    week 03

---
+++ Week_03/G20200343030587/src/main/java/com/home/work/week03/LeetCode_122_587.java
@@ -0,0 +1,14 @@
+package com.home.work.week03;
+
+public class LeetCode_122_587 {
+
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
+}
