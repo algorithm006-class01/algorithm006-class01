567619d6011cd1204bde68c8de12297774cb18c3
zhang teng <1299220775@qq.com>
2020-03-01 21:22:29 +0800

    441-Week 03

---
+++ Week_03/G20200343030441/LeetCode_127_441.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=127 lang=java
+ *
+ * [127] 单词接龙
+ */
+
+// @lc code=start
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+    }
+}
+// @lc code=end
+
