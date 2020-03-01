2de74117ec11e4d18347ca97395e2bcc529fcb0b
杨润炜 <runweiyang@tuputech.com>
2020-03-01 17:49:03 +0800

    refactor: 调整可读性,  LeetCode_33_387 NOTE.md to week03

--- Week_03/G20200343030387/LeetCode_33_387.js
+++ Week_03/G20200343030387/LeetCode_33_387.js
@@ -5,9 +5,9 @@
  */
 // 二分查找
 // 元素经过旋转后，不再是全局单调递增，但旋转点的左右部分还是递增的，且递增方向一致；
-// [nums[0], mid]
-//   如果 nums[0] < mid，表示这部分递增，target < nums[0] 或 target > mid，命中后半部分；
-//   如果 nums[0] > mid，表示这部分不是递增，target < nums[0] 且 target > mid，命中后半部分；
+// [nums[left], mid]
+//   如果 nums[left] < mid，表示这部分递增，target < nums[left] 或 target > mid，命中后半部分；
+//   如果 nums[left] > mid，表示这部分不是递增，target < nums[left] 且 target > mid，命中后半部分；
 // 否则，命中前半部分；
 // 排除到只剩下一个元素时，判断其值是否与target相等，是则返回其索引，否则返回-1
 var search = function (nums, target) {
@@ -15,15 +15,18 @@ var search = function (nums, target) {
     let left = 0
     let right = nums.length - 1
     let mid = 0
-    while (left < right) {
+    while (left <= right) {
         mid = parseInt((left + right) / 2)
-        if (nums[0] <= nums[mid] && (target < nums[0] || target > nums[mid])) {
+        if (nums[mid] === target) {
+            return mid
+        }
+        if (nums[left] <= nums[mid] && (target < nums[left] || target > nums[mid])) {
             left = mid + 1
-        } else if (nums[0] > target && target > nums[mid]) {
+        } else if (nums[left] > target && target > nums[mid]) {
             left = mid + 1
         } else {
-            right = mid
+            right = mid - 1
         }
     }
-    return left === right && nums[left] === target ? left : -1
+    return -1
 };
