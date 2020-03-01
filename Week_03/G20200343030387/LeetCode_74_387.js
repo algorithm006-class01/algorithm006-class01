0faa2a8e70a72c29b4d60ea95fdd9e00586ea98e
杨润炜 <runweiyang@tuputech.com>
2020-03-01 19:21:40 +0800

    feat: add LeetCode_153_387 NOTE.md to week03

--- Week_03/G20200343030387/LeetCode_74_387.js
+++ Week_03/G20200343030387/LeetCode_74_387.js
@@ -57,14 +57,3 @@ var searchMatrix = function (matrix, target) {
     }
     return false
 }
-
-console.log(searchMatrix([
-    [1, 3, 5, 7],
-    [10, 11, 16, 20],
-    [23, 30, 34, 50]
-], 3))
-console.log(searchMatrix([
-    [1, 3, 5, 7],
-    [10, 11, 16, 20],
-    [23, 30, 34, 50]
-], 13))
