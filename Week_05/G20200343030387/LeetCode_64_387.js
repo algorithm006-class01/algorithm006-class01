fe89cff6ccfab110229064f206404577cf3f2da7
杨润炜 <runweiyang@tuputech.com>
2020-03-10 13:40:05 +0800

    refactor: add another fn to 64

--- Week_05/G20200343030387/LeetCode_64_387.js
+++ Week_05/G20200343030387/LeetCode_64_387.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 // 二维DP方程： dp[i,j] = g[i,j] + min(dp[i+1, j], dp[i, j+1])
-var minPathSum = function (grid) {
+var minPathSum1 = function (grid) {
     const m = grid.length - 1
     const n = grid[0].length - 1
     const dp = grid
@@ -22,3 +22,24 @@ var minPathSum = function (grid) {
     }
     return dp[0][0]
 };
+
+// 一维DP方程： dp[j] = g[i,j] + min(dp[j], dp[j+1])
+var minPathSum = function (grid) {
+    const m = grid.length - 1
+    const n = grid[0].length - 1
+    const dp = []
+    for (let i = m; i >= 0; i--) {
+        for (let j = n; j >= 0; j--) {
+            if (i === m && j === n) {
+                dp[j] = grid[i][j]
+            } else if (i === m && j !== n) {
+                dp[j] = dp[j + 1] + grid[i][j]
+            } else if (i !== m && j === n) {
+                dp[j] += grid[i][j]
+            } else {
+                dp[j] = grid[i][j] + Math.min(dp[j], dp[j + 1])
+            }
+        }
+    }
+    return dp[0]
+}
