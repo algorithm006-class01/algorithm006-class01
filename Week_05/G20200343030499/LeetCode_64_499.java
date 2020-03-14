aa2d3c36e7e81e50b194ea354d708351be2b8150
Peng Wu <pengwu8421@gmail.com>
2020-03-14 02:26:03 -0700

    week5 initial commit

---
+++ Week_05/G20200343030499/LeetCode_64_499.java
@@ -0,0 +1,24 @@
+class Solution {
+    Set<String> codeSet = new HashSet<>(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
+            "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26"));
+    private int res = 0;
+
+    public int numDecodings(String s) {
+        int len = s.length();
+        int[] dp = new int[3];
+        dp[0] = 1;
+        dp[1] = codeSet.contains(s.substring(0, 1)) ? 1 : 0;
+
+        for (int i = 2; i <= len; i++) {
+            int currSolutionCount = 0;
+            if (codeSet.contains(s.substring(i - 1, i))) {
+                currSolutionCount += dp[(i - 1) % 3];
+            }
+            if (codeSet.contains(s.substring(i - 2, i))) {
+                currSolutionCount += dp[(i - 2) % 3];
+            }
+            dp[i % 3] = currSolutionCount;
+        }
+        return dp[len % 3];
+    }
+}
