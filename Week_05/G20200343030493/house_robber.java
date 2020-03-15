73de46ccc3c0f6268e965b69d3d2387ee9d4c51c
gaoyishu <gaoyishu91@163.com>
2020-03-15 23:53:55 +0800

    weeok05

---
+++ Week_05/G20200343030493/house_robber.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int rob(int[] num) {
+        int prevMax = 0;
+        int currMax = 0;
+        for (int x : num) {
+            int temp = currMax;
+            currMax = Math.max(prevMax + x, currMax);
+            prevMax = temp;
+        }
+        return currMax;
+    }
+}
