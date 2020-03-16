3c7b51ff625e96580f72f4219fedb0383c49d3d3
Willsgao <43256529+Willsgao@users.noreply.github.com>
2020-03-16 11:57:27 +0800

    417-week02
    change1

--- Week_02/G20200343030417/homework.py
+++ Week_02/G20200343030417/homework.py
@@ -19,15 +19,3 @@ def groupAnagrams(strs):
 
 # groupAnagrams(strs)
 
-# 二叉树前序遍历
-def preorder_tree(root):
-    res = []
-    p = root
-    stack = []
-    while p or stack:
-        while p:
-            res.append(p.val)
-            stack.append(p)
-            p = p.left
-        p = stack.pop().right
-    return res
