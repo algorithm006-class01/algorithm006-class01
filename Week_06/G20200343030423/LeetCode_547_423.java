674424d74c7a3498644fd55ecc0d64e6c73b533a
xufei <1842070912@qq.com>
2020-03-22 22:13:38 +0800

    423第五周作业

---
+++ Week_06/G20200343030423/LeetCode_547_423.java
@@ -0,0 +1,25 @@
+public class LeetCode_547_423 {
+}
+
+class Solution547 {
+    public int findCircleNum(int[][] M) {
+        boolean[] visited = new boolean[M.length];
+        int ans = 0;
+        for(int i = 0; i < M.length; i++) {
+            if(!visited[i]) {
+                ans++;
+                dfs(M, i, visited);
+            }
+        }
+        return ans;
+    }
+
+    private void dfs(int[][] M, int i, boolean[] visited) {
+        visited[i] = true;
+        for(int j = 0; j < M.length; j++) {
+            if(!visited[j] && M[i][j] == 1) {
+                dfs(M, j, visited);
+            }
+        }
+    }
+}
