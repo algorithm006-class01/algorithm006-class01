22508a8fcf94e92fd0ea3b792995f79404933993
MurphyArc <mulingsa@gmail.com>
2020-03-22 23:14:31 +0800

    527-Week 06

---
+++ Week_05/G20200343030527/LeetCode_221_527.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int rows = matrix.length;
+        int cols;
+        if (rows > 0) {
+            cols = matrix[0].length;
+        }else {
+            cols = 0;
+        }
+        int[][] dp = new int[rows + 1][cols + 1];
+        int max = 0;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (matrix[i-1][j-1] == '1'){
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        return max * max;
+    }
+}
---
+++ Week_06/G20200343030527/LeetCode_70_527.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int s1 = 1;
+        int s2 = 2;
+        for (int i = 3; i <= n; ++i) {
+            int temp = s1 + s2;
+            s1 = s2;
+            s2 = temp;
+        }
+        return s2;
+    }
+}
