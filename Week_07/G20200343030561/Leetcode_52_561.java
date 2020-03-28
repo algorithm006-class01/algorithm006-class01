9d25239d0d2c3262ed97418911bd1133a8a79b45
1yx <onewhyaxe@gmail.com>
2020-03-28 16:49:20 +0800

    replace 52

--- Week_07/G20200343030561/Leetcode_52_561.java
+++ Week_07/G20200343030561/Leetcode_52_561.java
@@ -1,46 +1,70 @@
 /*
- * @lc app=leetcode.cn id=529 lang=java
+ * @lc app=leetcode.cn id=52 lang=java
  *
- * [529] 扫雷游戏
+ * [52] N皇后 II
  */
 
+
+// class Solution {
+//     int res = 0;
+//     public int totalNQueens(int n) {
+//         if (n == 0) return 0;
+//         dfs(n, 0, 0, 0, 0);
+//         return res;
+//     }
+//     void dfs(int n, int r, int c, int lr, int ll) { // row, col, lower right, lower left
+//         if (r == n) {
+//             res ++;
+//             return;
+//         }
+//         int bit = (~ (c | lr | ll)) & ((1 << n) - 1);
+//         while (bit != 0) {
+//             int q = bit & -bit;
+//             dfs(n, r+1, c|q, (lr|q)>>1, (ll|q)<<1);
+//             bit &= bit - 1;
+//         }
+//     }
+// }
+
+// class Solution {
+//     int res = 0;
+//     public int totalNQueens(int n) {
+//         if (n == 0) return 0;
+//         dfs((1 << n) - 1, 0, 0, 0);
+//         return res;
+//     }
+//     void dfs(int n, int c, int lr, int ll) { // col, lower right, lower left
+//         if (c == n) {
+//             res ++;
+//             return;
+//         }
+//         int bit = (~ (c | lr | ll)) & n;
+//         while (bit != 0) {
+//             int q = bit & -bit;
+//             dfs(n, c|q, (lr|q)>>1, (ll|q)<<1);
+//             bit &= bit - 1;
+//         }
+//     }
+// }
+
 // @lc code=start
+// @date Mar 28 2020
 class Solution {
-    public char[][] updateBoard(char[][] board, int[] click) {
-        int nr = board.length, nc = board[0].length;
-        int row = click[0], col = click[1];
-
-        if(board[row][col] == 'M')
-            board[row][col] = 'X';
-        else {
-            int count = 0;
-            for (int i = -1; i < 2; i ++) {
-                for (int j = -1; j < 2; j ++){
-                    if (i == 0 && j == 0) continue;
-                    int r = row + i, c = col + j;
-                    if (r<0||c<0||r>=nr||c>=nc) continue;
-                    if (board[r][c] == 'M')
-                        count ++;
-                }
-            }
-
-            if (count > 0) 
-                board[row][col] = (char)(count + 48); // '0' ascii code 
-            else {
-                board[row][col] = 'B';
-                for (int i = -1; i < 2; i ++) {
-                    for (int j = -1; j < 2; j ++){
-                        if (i == 0 && j == 0) continue;
-                        int r = row + i, c = col + j;
-                        if (r<0||c<0||r>=nr||c>=nc) continue;
-                        if (board[r][c] == 'E')
-                            updateBoard(board, new int[]{r, c});
-                    }
-                }   
-            }            
-            
+    int res = 0;
+    public int totalNQueens(int n) {
+        if (n == 0) return 0;
+        dfs((1 << n) - 1, 0, 0, 0);
+        return res;
+    }
+    void dfs(int b, int c, int lr, int ll) { // board, column, lower right, lower left
+        if (c == b) {
+            res ++;
+            return;
         }
-        return board;
+        for (int qs = (~ (c | lr | ll)) & b; qs != 0; qs &= qs - 1) {
+            int q = qs & (- qs);
+            dfs(b, c | q, (lr | q) >> 1, (ll | q) << 1);
+        }
     }
 }
 // @lc code=end
