4ab5295e81cccda654ef8758767dd4822c221aaf
pyluyang <1065311304@qq.com>
2020-03-29 21:04:45 +0800

    算法训练营第七周作业

---
+++ Week_07/G20200343030537/leetcode_52_537.py
@@ -0,0 +1,54 @@
+#
+# @lc app=leetcode.cn id=52 lang=python3
+#
+# [52] N皇后 II
+#
+
+# @lc code=start
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        """
+        :type n: int
+        :rtype: int
+        """
+        def backtrack(row = 0, hills = 0, next_row = 0, dales = 0, count = 0):
+            """
+            :type row: 当前放置皇后的行号
+            :type hills: 主对角线占据情况 [1 = 被占据，0 = 未被占据]
+            :type next_row: 下一行被占据的情况 [1 = 被占据，0 = 未被占据]
+            :type dales: 次对角线占据情况 [1 = 被占据，0 = 未被占据]
+            :rtype: 所有可行解的个数
+            """
+            if row == n:  # 如果已经放置了 n 个皇后
+                count += 1  # 累加可行解
+            else:
+                # 当前行可用的列
+                # ! 表示 0 和 1 的含义对于变量 hills, next_row and dales的含义是相反的
+                # [1 = 未被占据，0 = 被占据]
+                free_columns = columns & ~(hills | next_row | dales)
+                
+                # 找到可以放置下一个皇后的列
+                while free_columns:
+                    # free_columns 的第一个为 '1' 的位
+                    # 在该列我们放置当前皇后
+                    curr_column = - free_columns & free_columns
+                    
+                    # 放置皇后
+                    # 并且排除对应的列
+                    free_columns ^= curr_column
+                    
+                    count = backtrack(row + 1, 
+                                      (hills | curr_column) << 1, 
+                                      next_row | curr_column, 
+                                      (dales | curr_column) >> 1, 
+                                      count)
+            return count
+
+        # 棋盘所有的列都可放置，
+        # 即，按位表示为 n 个 '1'
+        # bin(cols) = 0b1111 (n = 4), bin(cols) = 0b111 (n = 3)
+        # [1 = 可放置]
+        columns = (1 << n) - 1
+        return backtrack()
+# @lc code=end
+
