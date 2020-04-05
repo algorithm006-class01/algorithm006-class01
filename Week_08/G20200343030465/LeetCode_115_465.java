d769904608cd68987a1074f7c4de3ca47d20d4ad
ScarletSheep943 <876621152@qq.com>
2020-04-05 12:00:19 +0800

    week 8

---
+++ Week_08/G20200343030465/LeetCode_115_465.java
@@ -0,0 +1,7 @@
+class Solution {
+   public String reverseWords(String s) {
+    String[] words = s.trim().split(" +");
+    Collections.reverse(Arrays.asList(words));
+    return String.join(" ", words);
+}
+}
